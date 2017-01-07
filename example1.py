b49c39843c475bf12c2d0ee7f43711e5cf49373b
Alfredo Fanghella <alfredojfv.94@gmail.com>
2017-01-07 21:04:59 +0100

    added linear PCA to example 1

--- example1.py
+++ example1.py
@@ -1,3 +1,4 @@
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import numpy as np
 import our_kpca
@@ -6,10 +7,20 @@ import data
 for shape in (data.get_curves, data.get_square):
     X, Y = shape(noise='normal', scale=0.2)
     noisy_data = np.array([X, Y]).T
+
     kpca = our_kpca.kPCA(noisy_data, noisy_data)
     denoised = kpca.obtain_preimages(4, 0.6)
-
     plt.hold(True)
+    plt.subplot(1, 2, 1)
     plt.plot(X, Y, 'k.')
+    plt.title('Kernel PCA')
+    plt.plot(denoised[:,0], denoised[:,1], 'r.')
+
+    pca = PCA(n_components=1)
+    new_representation = pca.fit_transform(noisy_data)
+    denoised = pca.inverse_transform(new_representation)
+    plt.subplot(1, 2, 2)
+    plt.plot(X, Y, 'k.')
+    plt.title('Linear PCA')
     plt.plot(denoised[:,0], denoised[:,1], 'r.')
     plt.show()
