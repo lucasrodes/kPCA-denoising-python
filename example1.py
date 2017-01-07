93f5a501e320e1bb307fb627ffb7b061cdae8876
Alfredo Fanghella <alfredojfv.94@gmail.com>
2017-01-07 19:46:36 +0100

    New (provisional) value for c in example 1

--- example1.py
+++ example1.py
@@ -7,7 +7,7 @@ for shape in (data.get_curves, data.get_square):
     X, Y = shape(noise='normal', scale=0.2)
     noisy_data = np.array([X, Y]).T
     kpca = our_kpca.kPCA(noisy_data, noisy_data)
-    denoised = kpca.obtain_preimages(4, 1)
+    denoised = kpca.obtain_preimages(4, 0.4)
 
     plt.hold(True)
     plt.plot(X, Y, 'k.')
