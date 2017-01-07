ab9b45969b0838f4db6ee514d7a0498a28edaff5
Alfredo Fanghella <alfredojfv.94@gmail.com>
2017-01-07 19:33:43 +0100

    Added square to example 1

--- data.py
+++ data.py
@@ -39,10 +39,57 @@ def get_curves(points=1000, radius=2, noise=None, *args, **kwargs):
     right_y = right_radius*sin[::-1]
     return np.concatenate((left_x, right_x)), np.concatenate((left_y, right_y))
 
+def get_square(points=1000, length=4, noise=None, *args, **kwargs):
+    """
+    Generates syntethic data in the shape of two quarter circles, as in
+    the example from the paper by Mika et al.
+
+    Arguments:
+        points: number of points in the generated dataset.
+        noise: name of the distribution to be used as additive noise.
+               Use one of the distribution from numpy.random, see
+               https://docs.scipy.org/doc/numpy-1.10.0/reference/routines.random.html
+               Default is 'uniform', with low=-0.5 and high=0.5. Noise is added
+               in the direction orthogonal to the current side.
+        args, kwargs: Any arguments you want to pass to the corresponding
+                      numpy.random sampling function, except size.
+
+    Returns:
+        Arrays with the X and Y coordinates for the new data.
+    """
+    if noise is None:
+        noise = 'uniform'
+        kwargs['low'] = -0.5
+        kwargs['high'] = 0.5
+    kwargs['size'] = points // 4
+    dist = getattr(np.random, noise)
+
+    real_values = np.linspace(0, length, num=points//4)
+    x_values = []
+    y_values = []
+    # Left side
+    x_values.append(dist(*args, **kwargs))
+    y_values.append(real_values)
+    # Right side
+    x_values.append(dist(*args, **kwargs) + length)
+    y_values.append(real_values)
+    # Top side
+    x_values.append(real_values)
+    y_values.append(dist(*args, **kwargs) + length)
+    # Bottom side
+    x_values.append(real_values)
+    y_values.append(dist(*args, **kwargs))
+
+    return np.concatenate(x_values), np.concatenate(y_values)
+
+
 def main():
     "Example data generation"
     X, Y = get_curves()
     import matplotlib.pyplot as plt
+    plt.plot(X, Y ,'k.')
+    plt.show()
+    X, Y = get_square()
     plt.plot(X, Y ,'k.')
     plt.show()
 
--- example1.py
+++ example1.py
@@ -3,12 +3,13 @@ import numpy as np
 import our_kpca
 import data
 
-X, Y = data.get_curves(noise='normal', scale=0.2)
-circles = np.array([X, Y]).T
-kpca = our_kpca.kPCA(circles, circles)
-denoised = kpca.obtain_preimages(4, 1)
+for shape in (data.get_curves, data.get_square):
+    X, Y = shape(noise='normal', scale=0.2)
+    noisy_data = np.array([X, Y]).T
+    kpca = our_kpca.kPCA(noisy_data, noisy_data)
+    denoised = kpca.obtain_preimages(4, 1)
 
-plt.hold(True)
-plt.plot(X, Y, 'k.')
-plt.plot(denoised[:,0], denoised[:,1], 'r.')
-plt.show()
+    plt.hold(True)
+    plt.plot(X, Y, 'k.')
+    plt.plot(denoised[:,0], denoised[:,1], 'r.')
+    plt.show()
