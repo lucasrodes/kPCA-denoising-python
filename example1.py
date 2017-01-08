603d1ce5cb2cfede06bd6b5a36f88ccde5518196
Alfredo Fanghella <alfredojfv.94@gmail.com>
2017-01-08 02:04:44 +0100

    Code refactoring and improvements
    Commented the princurves module.
    Avoided some of the repetition in example 1.

--- example1.py
+++ example1.py
@@ -1,66 +1,48 @@
 from sklearn.decomposition import PCA
+from our_kpca import kPCA
 from princurves import fit_curve
 import matplotlib.pyplot as plt
 import numpy as np
 import our_kpca
 import data
 
+def show_plot(methods):
+    "Plots all results in the input list as a series of subplots"
+    n_methods = len(methods)
+    i = 1
+    plt.hold(True)
+    for denoised, name in methods:
+        plt.subplot(1, n_methods, i)
+        plt.plot(X, Y, 'k.')
+        plt.title(name)
+        plt.plot(denoised[:,0], denoised[:,1], 'r.')
+        i += 1
+    plt.show()
+
+def pca_denoising(data):
+    "Performs linear PCA denoising using sklearn"
+    pca = PCA(n_components=1)
+    low_dim_representation = pca.fit_transform(data)
+    return pca.inverse_transform(low_dim_representation)
+
+# To add a new method, simply add it to both methods list
 # Curves
 X, Y = data.get_curves(noise='normal', scale=0.2)
 noisy_data = np.array([X, Y]).T
-
-kpca = our_kpca.kPCA(noisy_data, noisy_data)
-denoised = kpca.obtain_preimages(4, 0.6)
-plt.hold(True)
-plt.subplot(1, 3, 1)
-plt.plot(X, Y, 'k.')
-plt.title('Kernel PCA')
-plt.plot(denoised[:,0], denoised[:,1], 'r.')
-
-denoised = fit_curve(noisy_data)
-plt.subplot(1, 3, 2)
-plt.plot(X, Y, 'k.')
-plt.title('Principal Curves')
-plt.plot(denoised[:,0], denoised[:,1], 'r.')
-
-pca = PCA(n_components=1)
-new_representation = pca.fit_transform(noisy_data)
-denoised = pca.inverse_transform(new_representation)
-plt.subplot(1, 3, 3)
-plt.plot(X, Y, 'k.')
-plt.title('Linear PCA')
-plt.plot(denoised[:,0], denoised[:,1], 'r.')
-plt.show()
+methods = [
+    (kPCA(noisy_data, noisy_data).obtain_preimages(4, 0.5), 'Kernel PCA'),
+    (fit_curve(noisy_data), 'Principal Curves'),
+    (pca_denoising(noisy_data), 'Linear PCA')
+]
+show_plot(methods)
 
 # Square
-X, Y = data.get_square()
+X, Y = data.get_square(noise='normal', scale=0.2)
 noisy_data = np.array([X, Y]).T
-
-kpca = our_kpca.kPCA(noisy_data, noisy_data)
-denoised = kpca.obtain_preimages(4, 0.6)
-plt.hold(True)
-plt.subplot(1, 4, 1)
-plt.plot(X, Y, 'k.')
-plt.title('Kernel PCA')
-plt.plot(denoised[:,0], denoised[:,1], 'r.')
-
-denoised = fit_curve(noisy_data)
-plt.subplot(1, 4, 2)
-plt.plot(X, Y, 'k.')
-plt.title('Principal Curves')
-plt.plot(denoised[:,0], denoised[:,1], 'r.')
-
-denoised = fit_curve(noisy_data, circle=True)
-plt.subplot(1, 4, 3)
-plt.plot(X, Y, 'k.')
-plt.title('Principal Curves (starting from a circle)')
-plt.plot(denoised[:,0], denoised[:,1], 'r.')
-
-pca = PCA(n_components=1)
-new_representation = pca.fit_transform(noisy_data)
-denoised = pca.inverse_transform(new_representation)
-plt.subplot(1, 4, 4)
-plt.plot(X, Y, 'k.')
-plt.title('Linear PCA')
-plt.plot(denoised[:,0], denoised[:,1], 'r.')
-plt.show()
+methods = [
+    (kPCA(noisy_data, noisy_data).obtain_preimages(4, 0.6), 'Kernel PCA'),
+    (fit_curve(noisy_data), 'Principal Curves'),
+    (fit_curve(noisy_data, circle=True), 'Principal Curves (starting from a circle)'),
+    (pca_denoising(noisy_data), 'Linear PCA')
+]
+show_plot(methods)
--- princurves.py
+++ princurves.py
@@ -10,6 +10,16 @@ princurve = importr('princurve')
 
 def fit_curve(
     data, circle=False, iterations=500, stretch=None, threshold=0.00001):
+    """
+    :param data: numpy array, shape (n_samples, n_features), to be denoised
+    :param circle: True if fitting starts with a circle, usefull for denoising closed curves
+    :param iterations: maximum number of iterations
+    :param stretch: parameter that affects curve extrapolation
+    :param threshold: convergence threshold on shortest distances to the curve
+    :returns: denoised data in numpy array with shape (n_samples, n_features)
+    """
+    # For more information see:
+    # https://cran.r-project.org/web/packages/princurve/princurve.pdf
     numpy2ri.activate()
     if circle:
         smoother = 'periodic.lowess'
