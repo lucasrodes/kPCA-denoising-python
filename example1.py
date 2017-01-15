55f92062ec2c6187988345ca9a31358afe038d67
Alfredo Fanghella <alfredojfv.94@gmail.com>
2017-01-15 18:53:15 +0100

    Better figure for example 1 slides
    - Original data in gray, de-noised data in black, more similar to the paper
    - Changed height of half circles to half of the size of the squares

--- example1.py
+++ example1.py
@@ -10,17 +10,17 @@ import data
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-def plot(methods, X, Y, line):
+def plot(methods, X, Y, line, rowspan):
     "Plots all results in the input list as a series of subplots"
     n_methods = len(methods)
-    i = 1
+    i = 0
     plt.hold(True)
     handles = []
     for denoised, name in methods:
-        plt.subplot(2, 4, i + 4*line)
-        handle1, = plt.plot(X, Y, 'k.')
+        plt.subplot2grid((3, 4), (line, i), rowspan=rowspan)
+        handle1, = plt.plot(X, Y, '.', color="0.8")
         plt.title(name)
-        handle2, = plt.plot(denoised[:,0], denoised[:,1], 'r.')
+        handle2, = plt.plot(denoised[:,0], denoised[:,1], 'k.')
         i += 1
         handles.append(handle1)
         handles.append(handle2)
@@ -41,7 +41,7 @@ methods = [
     (fit_curve(noisy_data), 'Principal Curves'),
     (pca_denoising(noisy_data), 'Linear PCA')
 ]
-plot(methods, X, Y, 0)
+plot(methods, X, Y, 0, 1)
 
 # Square
 X, Y = data.get_square(noise='normal', scale=0.2)
@@ -52,7 +52,7 @@ methods = [
     (fit_curve(noisy_data, circle=True), 'Principal Curves (from circle)'),
     (pca_denoising(noisy_data), 'Linear PCA')
 ]
-handles = plot(methods, X, Y, 1)
+handles = plot(methods, X, Y, 1, 2)
 plt.figlegend(handles[:2], ['Original data', 'Denoised data'],
-    loc='upper right', bbox_to_anchor=(0.95, 0.75))
+    loc='upper right', bbox_to_anchor=(0.92, 0.85))
 plt.show()
